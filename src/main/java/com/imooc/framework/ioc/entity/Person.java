4899cf66c1576e3d06498183784b2c7f26c93a95
luzhiyuan <yuanbao0228@gmail.com>
2019-04-18 18:08:35 +0800

    first commit

---
+++ src/main/java/com/imooc/framework/ioc/entity/Person.java
@@ -0,0 +1,38 @@
+package com.imooc.framework.ioc.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("person")
+public class Person {
+
+    @Value("1")
+    private Long id;
+
+    @Value("aaa")
+    private String name;
+
+    @Autowired
+    private Pet pet;
+
+    public void call() {
+        pet.move();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
